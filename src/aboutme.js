0d7b759712272bf63e6a7c8c72fc3bb039b3590c
stephooooo <stephaniembaabu@gmail.com>
2022-12-06 17:07:13 +0300

    pics

--- src/aboutme.js
+++ src/aboutme.js
@@ -14,31 +14,31 @@ function Aboutme
 
 <div className='md:columns-1 lg:columns-3 space-y-4'>
 <div>
-<img src='../img/10.JPEG' />
+<img src='../img/10.png' />
 </div>
 <div>
-<img src='../img/11.JPEG' />
+<img src='../img/11.png' />
 </div>
 <div>
-<img src='../img/12.JPEG' />
+<img src='../img/12.png' />
 </div>
 <div>
-<img src='../img/13.JPEG' />
+<img src='../img/13.png' />
 </div>
 <div>
-<img src='../img/14.JPEG' />
+<img src='../img/14.png' />
 </div>
 <div>
-<img src='../img/15.JPEG' />
+<img src='../img/15.png' />
 </div>
 <div>
-<img src='../img/16.JPEG' />
+<img src='../img/16.png' />
 </div>
 <div> 
-<img src='../img/17.JPEG' />
+<img src='../img/17.png' />
 </div>
 <div>
-<img src='../img/18.JPEG' />
+<img src='../img/18.png' />
 </div>
 
 </div>
